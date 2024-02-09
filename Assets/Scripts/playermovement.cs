2331dced1b1fc795c3af846f4c78ffaa93ef3541
Nathaniel Bare <nathaniel.d.bare@gmail.com>
2024-02-09 17:20:07 -0500

    Test edits on Bare scene

---
+++ Assets/Scripts/playermovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I am Alive and my name is chilli pepper");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
