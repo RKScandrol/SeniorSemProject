542fcf95637b4f02e754fc872c945185cad639c6
Cole Tucker <coletucker2021@gmail.com>
2024-02-24 12:43:57 -0500

    Simple scene transitions implemented

--- Assets/Scripts/Transition/SceneChange_Sleep.cs
+++ Assets/Scripts/Transition/SceneChange_Sleep.cs
@@ -7,12 +7,18 @@ public class SceneChange : MonoBehaviour
 {
     //Used to specify target scene
     public int sceneBuildIndex;
+    public Animator animator;
+    public BoxCollider2D boxCollider2D;
 
     private void OnTriggerEnter2D(Collider2D other) {
-        //Player entered trigger point, transition to next scene
+        //Player entered trigger point, initiate transition to next scene
         if (other.tag == "Player") {
-            SceneManager.LoadSceneAsync(sceneBuildIndex, LoadSceneMode.Single);
+            animator.SetTrigger("FadeOut");
         }
+    }
+
+    public void OnFadeComplete() {
+        SceneManager.LoadSceneAsync(sceneBuildIndex, LoadSceneMode.Single);
     }
     
 }
