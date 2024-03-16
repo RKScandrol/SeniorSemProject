58e822f8f05795b3e172e81907c6382d1ba8c354
AlexMakesCrabs <nathaniel.d.bare@gmail.com>
2024-03-16 16:41:32 -0400

    added skeleton for attribute modifier to player script

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -8,8 +9,14 @@ public class Player : MonoBehaviour
     Rigidbody2D rb;
     float horizontal;
     float vertical;
-    public float moveSpeed = 20.0f;
     float moveLimiter = 0.7f;
+
+    // Player attributes
+    public float moveSpeed = 20.0f;
+    public float maxHealth = 100.0f;
+    public float currentHealth;
+    public float damage = 10.0f;
+
 
     //Trying to (mostly) handle animations in code, let's see how it goes!
     public Animator animator;
@@ -18,6 +25,12 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Set all player attributes to their proper values
+        modifyDamage(getPermStatus("damage"));
+        modifyMaxHealth(getPermStatus("maxHealth"));
+        modifyMoveSpeed(getPermStatus("moveSpeed"));
+        setCurrentHealth(maxHealth);
+
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -58,6 +71,50 @@ public class Player : MonoBehaviour
         else if (rb.velocity.x > 0)
         {
             animator.Play("guy_walk_R");
+        }
+    }
+
+    //Gets a value from Json as a float based on a given name of that attribute.
+    float getPermStatus(String statName)
+    {
+        //TODO: get JSON as float;
+
+        return 1;
+    }
+
+    //Modifies the movement speed of the character by multiplying it by some float value.
+    void modifyMoveSpeed(float mod)
+    {
+        moveSpeed *= mod;
+    }
+
+    //Modifies the base damage of the character by multiplying it by some float value.
+    void modifyDamage(float mod)
+    {
+        damage *= mod;
+    }
+
+    //Modifies the max health of the character my multiplying it by some float value.
+    void modifyMaxHealth(float mod)
+    {
+        maxHealth *= mod;
+    }
+
+    //Modifies the current health value by adding a certain float to it.
+    void modifyCurrentHealth(float mod)
+    {
+        currentHealth += mod;
+    }
+
+    //Sets the current health to a desired value (used mainly for initialization of the character.)
+    void setCurrentHealth(float val)
+    {
+        if(val>maxHealth)
+        {
+            currentHealth = maxHealth;
+        }else
+        {
+            currentHealth = val;
         }
     }
 }
