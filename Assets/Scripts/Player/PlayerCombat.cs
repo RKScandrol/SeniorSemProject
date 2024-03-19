e54723ae6b243ecc0228b6b9a08b9c73c2d70cb0
Cole Tucker <coletucker2021@gmail.com>
2024-03-19 16:59:33 -0400

    Created player combat manager script

---
+++ Assets/Scripts/Player/PlayerCombat.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public Transform attackPoint;
+    [SerializeField]
+    public float attackRange;
+    public float knockbackForce;
+    Rigidbody2D player;
+    public LayerMask enemyLayer;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Attack();
+        }
+    }
+
+    void Attack() {
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Vector2 direction = (enemy.transform.position - attackPoint.transform.position).normalized;
+            enemy.attachedRigidbody.AddForce(direction * knockbackForce);
+        }
+    }
+}
