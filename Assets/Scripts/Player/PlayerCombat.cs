990691a9b1a58fb9a439f020ddc48a204ff50f01
Ryan Scandrol <rkscandrol@gmail.com>
2024-04-10 20:45:44 -0400

    LifeSteal Item
    Added LifeSteal Item that heals the Player after attacking an enemy by a percent of the damage done

--- Assets/Scripts/Player/PlayerCombat.cs
+++ Assets/Scripts/Player/PlayerCombat.cs
@@ -15,6 +15,9 @@ public class PlayerCombat : MonoBehaviour
     public float attackStartup;
     public float attackDuration;
     public Animator animator;
+
+    private LifeSteal lifeSteal;
+
     
     void Start()
     {
@@ -37,6 +40,8 @@ public class PlayerCombat : MonoBehaviour
         player.constraints = RigidbodyConstraints2D.FreezeAll;
         animator.SetTrigger("Attacking");
         yield return new WaitForSeconds(attackStartup);
+        //Get Player Attack Attribute
+        attackDamage = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerAttributes>().attack;
 
         //After attack startup, damage and knockback applied to enemies within range
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
@@ -45,7 +50,14 @@ public class PlayerCombat : MonoBehaviour
         {
             Vector2 direction = (enemy.transform.position - attackPoint.transform.position).normalized;
             enemy.attachedRigidbody.AddForce(direction * knockbackForce);
-            enemy.gameObject.GetComponent<EnemyAttributes>().takeDamage(attackDamage);
+            int damageDealt = enemy.gameObject.GetComponent<EnemyAttributes>().takeDamage(attackDamage);
+
+            //If Player has an active LifeSteal Item
+            if (lifeSteal != null) {
+                //Give the DamageDealt to the Enemy to LifeSteal so that it will Heal the Player
+                lifeSteal.giveLifeToPlayer(damageDealt);
+            }
+            
         }
 
         //Waits for attack duration to complete
@@ -60,4 +72,15 @@ public class PlayerCombat : MonoBehaviour
     void OnDrawGizmosSelected() {
         Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
+
+
+
+    public void addLifeSteal(LifeSteal lf) {
+        lifeSteal = lf;
+    }
+    public void removeLifeSteal() {
+        lifeSteal = null;
+    }
+
+
 }
