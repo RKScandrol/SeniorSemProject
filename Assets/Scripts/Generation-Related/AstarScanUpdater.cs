47ce17c33e8228612be8c758aec5815f5181e0e5
James-JN20 <jamesnix888@gmail.com>
2024-04-17 20:32:20 -0400

    astar comp error fix

--- Assets/Scripts/Generation-Related/AstarScanUpdater.cs
+++ Assets/Scripts/Generation-Related/AstarScanUpdater.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class AstarScanUpdater : MonoBehaviour
 {
+    bool isUpdated = false;
     // Start is called before the first frame update
     void Start()
     {
-        updateScan();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isUpdated = false)
+        {
+            updateScan();
+            isUpdated = true;
+        }
     }
 
     public void updateScan()
